b8988a6a7e43067f26b5ce78d5f2de67f22cecef
maize <1522843099@qq.com>
2020-06-15 23:47:34 +0800

    demo

---
+++ src/cn/yz/demo.java
@@ -0,0 +1,14 @@
+package cn.yz;
+
+/**
+ * @ClassName demo
+ * @Description TODO
+ * @Author 苞谷洁子
+ * @Date 2020/6/15 23:44
+ * @Version 1.0
+ **/
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("helloWorld!");
+    }
+}
